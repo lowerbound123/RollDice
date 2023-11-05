a8644b70ba5ec7ebc1776807ab6f78a3887c5386
lowerbound123 <zhouyx2003@outlook.com>
2023-11-05 11:41:02 +0800

    fastfix

--- js/index.js
+++ js/index.js
@@ -4,8 +4,10 @@ function redirectToPage(x) {
 	}
 	if (x === "LoginPage") {
 		window.location.href = "LoginPage.html";
+		console.log("You tried to go to LoginPage")
 	}
 	if (x === "RegisterPage") {
 		window.location.href = "RegisterPage.html";
+		console.log("You tried to go to RegisterPage")
 	}
 }
