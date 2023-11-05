92eb75def49d81c88ada61484a04d356c83d8f6b
lowerbound123 <zhouyx2003@outlook.com>
2023-11-05 11:02:58 +0800

    add some simple items

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 function redirectToPage(x) {
-	if (x == "SimpleRoll") {
+	if (x === "SimpleRoll") {
 		window.location.href = "SimpleRoll.html"
+	}
+	if (x === "LoginPage") {
+		window.location.href = "LoginPage.html"
+	}
+	if (x === "RegisterPage") {
+		window.location.href = "RegisterPage.html"
 	}
 }
