cf21f3e8712234d55eb0680518ca6da165aec6d8
lowerbound123 <zhouyx2003@outlook.com>
2023-11-05 11:34:50 +0800

    fastfix

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 function redirectToPage(x) {
 	if (x === "SimpleRoll") {
-		window.location.href = "SimpleRoll.html"
+		window.location.href = "SimpleRoll.html";
 	}
 	if (x === "LoginPage") {
-		window.location.href = "LoginPage.html"
+		window.location.href = "LoginPage.html";
 	}
 	if (x === "RegisterPage") {
-		window.location.href = "RegisterPage.html"
+		window.location.href = "RegisterPage.html";
 	}
 }
