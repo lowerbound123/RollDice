bae6005e2a4ae4cac572c4414c12f3d45d17c493
lowerbound123 <zhouyx2003@outlook.com>
2023-11-06 22:56:12 +0800

    add basic.js

---
+++ js/basic.js
@@ -0,0 +1,11 @@
+function redirectToPage(x) {
+    if (x === "SimpleRoll") {
+        window.location.href = "SimpleRoll.html";
+    }
+    if (x === "LoginPage") {
+        window.location.href = "LoginPage.html";
+    }
+    if (x === "RegisterPage") {
+        window.location.href = "RegisterPage.html";
+    }
+}
